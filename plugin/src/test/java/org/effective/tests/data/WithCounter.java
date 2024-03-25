ebdacec78dc800eab27e612eab5e4474a09acf36
LOUISE COOKE <louisecynthiacooke@gmail.com>
2024-03-25 10:33:10 -0700

    Small test change

--- plugin/src/test/java/org/effective/tests/data/WithCounter.java
+++ plugin/src/test/java/org/effective/tests/data/WithCounter.java
@@ -3,7 +3,7 @@ package org.effective.tests.data;
 public class WithCounter {
     private int counter;
     public int incrementCounter() {
-        counter = 1;
+        counter = counter + 1;
         return counter;
     }
 }
